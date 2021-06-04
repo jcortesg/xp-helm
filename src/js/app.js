3de335b19c1dbf174486f1d558ab7fa2cf1e4447
Jhon Cortes <jhone.cortesg@gmail.com>
2021-06-04 17:10:32 -0500

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 import $ from 'jquery';
 
-import './module/cat';
-
 window.$ = $;
 window.jQuery = $;
+
+$('.mt-5').click(() => {
+    // eslint-disable-next-line
+    console.log('clicked on a cat photo2');
+});
