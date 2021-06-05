2d9abeedeb519a90b22b0a3250822c02da1c6262
Jhon Cortes <jhone.cortesg@gmail.com>
2021-06-05 12:48:56 -0500

    add

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,6 @@
-import $ from 'jquery';
-
-window.$ = $;
-window.jQuery = $;
-
-$('.mt-5').click(() => {
-    // eslint-disable-next-line
-    console.log('clicked on a cat photo2');
-});
+import 'bootstrap/js/dist/modal';
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/tab';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/alert';
